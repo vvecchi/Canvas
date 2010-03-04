59a9aaa1e9044862eed7754b410ea892e51c9e15
Vinicius <vvecchi@gmail.com>
2010-03-03 22:41:22 -0300

    ajustes no codigo

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 var COUNT = 0;
 var NUMSEGS = 5;
 var SEGSIZE = (240/2)/NUMSEGS;
-var SECONDS_BETWEEN_FRAMES = 1/60;
+var SECONDS_BETWEEN_FRAMES = 1000/60;
 var pos = 0;
 var velocity = 0;
 var maxVelocity = 200;
 var speedScale = 0.04
-lastFrame = new Date().getTime();
-var track = new Track();
 var isAccelerating = false;
 var isBreaking = false;
 var acceleration = 50;
@@ -16,60 +14,72 @@ var accelerateKeyDown = 97;
 var breakKeyDown = 122;
 var accelerateKeyUp = 65;
 var breakKeyUp = 90;
+var screenWidth = 640;
+var screenHeight = 480;
+
+lastFrame = new Date().getTime();
+horizon = screenHeight/2;
+yWorld = -80;
+var track = new Track(yWorld,horizon,screenWidth,screenHeight);
+var myCar = new MyCar(yWorld,horizon,screenWidth,screenHeight);
 
 
 function draw(){
-    track.draw();
+    var ctx = canvas.getContext('2d');
+    track.draw(canvas,ctx);
+    myCar.draw(canvas,ctx);
  }
  
  function update(){
     thisFrame = new Date().getTime();
     var dt = (thisFrame - this.lastFrame)/1000;
     this.lastFrame = thisFrame;
-	if(isAccelerating){
-		if(velocity < maxVelocity){
-			velocity += acceleration*dt;
-		}
-	}
-	if(isBreaking){
-		velocity -=  deceleration*dt
-		if(velocity < 0){
-			velocity = 0;
-		}
-	}
-	if(!isAccelerating && !isBreaking){
-		velocity -=  acceleration/2*dt
-		if(velocity < 0){
-			velocity = 0;
-		}
-	}
-	
+    if(isAccelerating){
+        if(velocity < maxVelocity){
+            velocity += acceleration*dt;
+        }
+    }
+    if(isBreaking){
+        velocity -=  deceleration*dt
+        if(velocity < 0){
+            velocity = 0;
+        }
+    }
+    if(!isAccelerating && !isBreaking){
+        velocity -=  acceleration/2*dt
+        if(velocity < 0){
+            velocity = 0;
+        }
+    }
+
     pos = pos + velocity * dt *speedScale;
-	track.myCar.isBreaking = isBreaking;
+    myCar.isBreaking = isBreaking;
     track.update(pos);
  }
  
  function init(){
+    canvas = document.getElementById('canvas');
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
     setInterval(function(){update();}, SECONDS_BETWEEN_FRAMES)
     setInterval(function(){draw();}, SECONDS_BETWEEN_FRAMES)
  }
 
-document.onkeypress =function(e) {
-	if(e.which == accelerateKeyDown){
-		isAccelerating = true;
-	}
-	else if(e.which = breakKeyDown){
-		isBreaking = true;
-	}
+document.onkeydown =function(e) {
+    if(e.which == accelerateKeyUp){
+        isAccelerating = true;
+    }
+    else if(e.which = breakKeyUp){
+        isBreaking = true;
+    }
 }
 
 document.onkeyup = function(e) {
-
-	if(e.which == accelerateKeyUp){
-		
-		isAccelerating = false;
-	}
-	if(e.which = breakKeyUp){
-		isBreaking = false;
-	}
+    if(e.which == accelerateKeyUp){
+        
+        isAccelerating = false;
+    }
+    if(e.which = breakKeyUp){
+        isBreaking = false;
+    }
 }
--- track.js
+++ track.js
@@ -5,33 +5,52 @@ function Segment(){
     this.sizeOnScreen = 0;
 }
 
-function MyCar(){
-    this.z = 0;
-	this.x = 160;
-	this.width = 94;
-	this.height = 56;
-	this.offsetX = 0;
-	this.offsetY = 0;
-	this.isBreaking
+function MyCar(yWorld,horizon){
+    this.z = 0.5;
+    this.x = 10;
+    this.width = 94;
+    this.height = 56;
+    this.offsetX = 0;
+    this.offsetY = 0;
+    this.isBreaking = false;
+    this.yWorld = yWorld;
+    this.horizon = horizon;
+    
+    
+    this.draw = function(canvas, ctx){
+        carimg = document.getElementById('car')
+        objy = canvas.height - ((this.yWorld/this.z) + (this.horizon))  - ((this.height/2) + 20)/this.z;
+        objx = canvas.width/2 - this.x/2/this.z
+        ctx.drawImage(carimg, this.offsetX,this.offsetY,this.width,this.height,objx - (this.width/2)/this.z , objy, (this.width)/this.z, (this.height)/this.z);
+        if(this.isBreaking){
+        /*    ctx.fillStyle = "rgba(256,0,0,0.6)";
+            ctx.fillRect (this.x - (this.width/2)/this.z + 20, objy + (2.75*this.height/4)/this.z, 20, 7);
+            ctx.fillRect (this.x + (this.width/2)/this.z - 34, objy + (2.75*this.height/4)/this.z, 20 , 7);*/
+        }
+    }
 }
 
-function Track(){
+function Track(yWorld,horizon, width, height){
     
     this.pos = 0;
-    this.horizon = 120;
-    
+    this.horizon = horizon;
+    this.height = height;
+    this.width = width;
     var lastPos = 0;
     var numsegs = 120;
-    var totalDist = 0;
-    var yWorld = - 80;
+    var totalDist = -yWorld;
     var printz = true;
     var zs = new Array()
-    for(i = 0; i < 240; i++){
-        zs[i] = yWorld/(i - (240/2));
+    for(i = 0; i < this.height; i++){
+        zs[i] = yWorld/(i - (this.height/2));
+        if(zs[i] < 0)
+        {
+            zs[i] = -zs[i];
+        }
     }
-    totalDist = - yWorld;
     var segsize = totalDist/numsegs;
     var segments = new Array();
+    
     for(i = 0;i < numsegs; i++){
         segment = new Segment();
         if(i == 0)segment.z = zs[239];
@@ -39,52 +58,35 @@ function Track(){
         segment.shaded = i%2;
         segments[i] = segment;
     }
+    
     var firstIndex = 0;
     var lastIndex = numsegs - 1;
     var firstSegSize = segsize - segments[firstIndex];
     
-    this.myCar = new MyCar();
-    this.myCar.z = segments[1].z + segsize/4;
-    this.myCar.z = 0.75;
-    this.draw = function(){
-        var canvas = document.getElementById('canvas');
-        var ctx = canvas.getContext('2d');
+    
+    this.draw = function(canvas,ctx){
         var img = document.getElementById('track');
         var imgdark = document.getElementById('trackdark');
         var y = canvas.height - 1;
         y = parseInt(y);
         ctx.drawImage(img,0, 0, img.width, 1, 0, 0, canvas.width, canvas.height);
-        for(i = 1; i < 20 ; i++){
+        for(i = 0; i < 20 ; i++){
             var curIndex = (i + firstIndex) % numsegs;
             var nextIndex = (curIndex + 1)%numsegs;
             var ynew = canvas.height - ((yWorld/segments[nextIndex].z) + this.horizon + 1);
             ynew = parseInt(ynew);
             var sizeOnScreen =  ynew - y;
-            var drawImg = img
+            var drawImg = img;
             if(segments[curIndex].shaded == 1){
                 drawImg = imgdark;
             }
-			
             for(ypos = y; ypos > y + sizeOnScreen;  ypos--){
+                track.x = canvas.width/2;
                 ctx.drawImage(drawImg,0,200,1,1,0,ypos,canvas.width,1);
-                ctx.drawImage(drawImg, 0, 220, drawImg.width,1, 160 - (canvas.width/2)/zs[ypos], ypos, canvas.width/zs[ypos],1);
+                ctx.drawImage(drawImg, 0, 220, drawImg.width,1, canvas.width/2 - (canvas.width/zs[ypos])/2, ypos, canvas.width/zs[ypos],1);
             }
             y = ynew;
         }
-        
-        objy = canvas.height - ((yWorld/this.myCar.z) + (this.horizon));
-        if(objy < this.horizon)
-        {
-            objy= this.horizon;
-        }
-        carimg = document.getElementById('car')
-		
-        ctx.drawImage(carimg, this.myCar.offsetX,this.myCar.offsetY,this.myCar.width,this.myCar.height,this.myCar.x - (this.myCar.width/2)/this.myCar.z , objy - (carimg.height/20)/this.myCar.z, (this.myCar.width)/this.myCar.z, (this.myCar.height)/this.myCar.z);
-		if(this.myCar.isBreaking){
-			ctx.fillStyle = "rgba(256,0,0,0.6)";
-			ctx.fillRect (this.myCar.x - (this.myCar.width/2)/this.myCar.z + 20, objy - 15, 20, 7);
-			ctx.fillRect (this.myCar.x + (this.myCar.width/2)/this.myCar.z - 34, objy - 15, 20 , 7);
-		}
     }
      
     this.update = function(pos){
@@ -95,14 +97,14 @@ function Track(){
         }
         firstSegSize += dPos;
         segments[firstIndex].z = zs[239];
-        while(segments[(firstIndex + 1)%numsegs].z < 0){
-			segments[firstIndex].z = segments[lastIndex].z + segsize;
+        while(segments[(firstIndex + 1)%numsegs].z < 0.1){
+            segments[firstIndex].z = segments[lastIndex].z + segsize;
             lastIndex = firstIndex;
             firstIndex = (firstIndex + 1) % numsegs;
             firstSegSize = segsize;
-		/*	if(canvas.height - ((yWorld/segments[nextIndex].z) + this.horizon + 1) < this.horizon){
-				alert("lastIndex = " + lastIndex + "firstIndex = " + firstIndex + )
-			}*/
+        /*	if(canvas.height - ((yWorld/segments[nextIndex].z) + this.horizon + 1) < this.horizon){
+                alert("lastIndex = " + lastIndex + "firstIndex = " + firstIndex + )
+            }*/
         }
     }
 }
